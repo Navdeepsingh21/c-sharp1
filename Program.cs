7dfa8ef9d15f9dc3466424ee2480fec5ac70701e
Admin <Admin@DESKTOP-POQE336>
2019-05-22 10:47:25 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-1 + 4 * 6
+            Console.WriteLine(-1 + 4 * 6);
+            //( 35+ 5 ) % 7
+            Console.WriteLine((35 + 5) % 7);
+            //14 + -4 * 6 / 11
+            Console.WriteLine(14 + -4 * 6 / 11);
+            //2 + 15 / 6 * 1 - 7 % 2
+            System.Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+}
